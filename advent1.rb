05fcf65cb4e6cc2082e8a27b1842e7033863a1b2
Oscar Cheng Yih <oscaryih@hotmail.com>
2017-01-01 19:07:05 -0800

    Complete advent challenge day 1

--- advent1.rb
+++ advent1.rb
@@ -3,10 +3,11 @@ def main(input)
 	coor = [0, 0]
 	dir = 'N'
 	input.each do |instr|
+		puts instr[1..-1]
 		dir = change_dir(dir, instr[0])
-		move(coor, dir, instr[1])
-
+		move(coor, dir, instr[1..-1])
 	end
+	answer = coor[0].abs + coor[1].abs
 end
 
 def change_dir(current_dir, new_dir)
@@ -45,9 +46,11 @@ def move(coor, dir, dist)
 	elsif dir == 'E'
 		coor[1] += dist
 	elsif dir == 'S'
-		coor[0]
+		coor[0] -= dist
+	else
+		coor[1] -= dist
 	end
+	coor
 end
 
-puts change_dir('E', 'R')
-puts change_dir('S', 'L')
+puts main("R1, L3, R5, R5, R5, L4, R5, R1, R2, L1, L1, R5, R1, L3, L5, L2, R4, L1, R4, R5, L3, R5, L1, R3, L5, R1, L2, R1, L5, L1, R1, R4, R1, L1, L3, R3, R5, L3, R4, L4, R5, L5, L1, L2, R4, R3, R3, L185, R3, R4, L5, L4, R48, R1, R2, L1, R1, L4, L4, R77, R5, L2, R192, R2, R5, L4, L5, L3, R2, L4, R1, L5, R5, R4, R1, R2, L3, R4, R4, L2, L4, L3, R5, R4, L2, L1, L3, R1, R5, R5, R2, L5, L2, L3, L4, R2, R1, L4, L1, R1, R5, R3, R3, R4, L1, L4, R1, L2, R3, L3, L2, L1, L2, L2, L1, L2, R3, R1, L4, R1, L1, L4, R1, L2, L5, R3, L5, L2, L2, L3, R1, L4, R1, R1, R2, L1, L4, L4, R2, R2, R2, R2, R5, R1, L1, L4, L5, R2, R4, L3, L5, R2, R3, L4, L1, R2, R3, R5, L2, L3, R3, R1, R3")
